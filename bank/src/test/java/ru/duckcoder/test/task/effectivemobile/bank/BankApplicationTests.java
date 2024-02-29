fcba37baf74e5c0f72b447c50e51c1f23094737c
kudrDaniel <kudr.daniel.vlad.ch@gmail.com>
2024-02-29 10:26:32 +0300

    Initial commit

---
+++ bank/src/test/java/ru/duckcoder/test/task/effectivemobile/bank/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.duckcoder.test.task.effectivemobile.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
